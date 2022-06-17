84680cc84ea1fcef54f004e856c727259dd4b406
Adil <mohsadil070@gmail.com>
2022-06-17 17:34:14 +0530

    class base view

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import Login
+from .views import Signup
+from .views import Logout
+from .views import Index
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('register', views.register, name="register"),
-    path('login/', views.loginPage, name="login"),
-    path('logout/', views.logoutUser, name="logout"),
+    path('', Index.as_view(), name="home"),
+    path('register', Signup.as_view(), name="register"),
+    path('login/', Login.as_view(), name="login"),
+    path('logout/', Logout.as_view(), name="logout"),
 ]
